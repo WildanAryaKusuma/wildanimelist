16fbd0474a2805bdc3bb1c0402cf84648258a468
WildanAryaKusuma <wildankusuma822@gmail.com>
2025-01-28 14:49:34 +0700

    deploy to vercel

--- prisma/migrations/20240519081051_modify_delete_year_field/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `anime_year` on the `collection` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `collection` DROP COLUMN `anime_year`;
